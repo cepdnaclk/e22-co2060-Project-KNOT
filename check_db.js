3bc1dd4e3aff8e1831539333241273023de89360
Ewmi Wijesinghe <e22443@eng.pdn.ac.lk>
2026-04-29 21:40:47 +0530

    Updated booking-admin folder with latest changes

--- check_db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2/promise');
-async function check() {
-  const pool = mysql.createPool({ host: 'localhost', user: 'root', database: 'knot_db' });
-  const [rows] = await pool.query('SELECT * FROM faults');
-  console.log(rows);
-  process.exit(0);
-}
-check();
