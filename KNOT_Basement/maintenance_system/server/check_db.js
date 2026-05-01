0aaefe02067637cfa55e9b3303d184f7b2d9990c
Chamudi Perera <e22280@eng.pdn.ac.lk>
2026-05-01 16:52:40 +0530

    Updated maintenance backend logic

--- KNOT_Basement/maintenance_system/server/check_db.js
+++ KNOT_Basement/maintenance_system/server/check_db.js
@@ -1,3 +1,4 @@
+// This file is used to check if the database connection is working and if the 'faults' table exists and can be queried.
 const mysql = require('mysql2/promise');
 async function check() {
   const pool = mysql.createPool({ host: 'localhost', user: 'root', database: 'knot_db' });
